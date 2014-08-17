20319063d939580b1b7fbf62346f95ab40066e80
phonyphonecall <shendrickso2@wisc.edu>
2014-08-17 13:50:29 -0500

    angularized index page

--- js/angular/home.js
+++ js/angular/home.js
@@ -1,17 +1,40 @@
 var stagegage = angular.module('stagegage', []);
 
-stagegage.controller('RankingsController', function ($scope) {
-  $scope.ranks = [
-    {'name': 'THE BEST BAND',
-     'number': '1'
- 	},
-    {'name': 'ALMOST BEST BAND',
-     'number': '2'
- 	},
-    {'name': 'EH THEY\'RE OK',
-     'number': '3'
- 	}
-  ];
+stagegage.controller('HomeController', function ($scope) {
+  $scope.highlight =
+    {'title': 'NEXT FESTIVAL',
+     'img': 'img/outside-lands.jpg',
+     'ranks': [
+        { 'name': 'THE BEST BAND', 
+          'genres' : ['rock', 'pop', 'electro'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        },
+        { 'name': 'ALMOST BEST BAND', 
+          'genres' : ['rock', 'metal', 'death metal'],
+          'festivals' : ['ultra music festival', 'outside lands', 'coachella']
+        }
+        ]
+    };
 
   $scope.genres = [
 	  {	'name':'ROCK' },
