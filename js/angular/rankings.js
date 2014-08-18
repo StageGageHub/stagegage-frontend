8f366aac79d4351cb3dc39e34013aef24d814090
phonyphonecall <shendrickso2@wisc.edu>
2014-08-17 19:50:07 -0500

    ranks can be filtered by genre

--- js/angular/rankings.js
+++ js/angular/rankings.js
@@ -65,5 +65,54 @@ stagegage.controller('RankingsController', function ($scope) {
   	'electronic',
   	'rap',
   	'country',
-  ]
+  ];
+
+  $scope.filteredGenres = [''];
+
+  $scope.addToFilteredGenres = function(genre) {
+  	console.log('adding genre filter: ' + genre);
+  	if($scope.filteredGenres.indexOf(genre) < 0)	
+  		$scope.filteredGenres.push(genre);
+  	console.log('filter added');
+  };
+
+  $scope.removeFromFilteredGenres = function(genre) {
+  	console.log('removing genre filter: ' + genre);
+  	$scope.filteredGenres.pop(genre);
+  	console.log('filter removed');
+  };
+});
+
+
+stagegage.filter('filterByGenre', function() {
+	return function(ranks, filteredGenres) {
+		// Don't filter if only the placeholder [''] is present
+		if(filteredGenres.length == 1)
+			return ranks;
+
+		console.log('attempting to filter ' + ranks.length + ' ranks');
+		var filtered = [];
+
+		console.log('genres in filter: ');
+		angular.forEach(filteredGenres, function(genre) {
+			console.log(genre);
+		});
+
+		angular.forEach(ranks, function(rank) {
+			var genres = rank.genres;
+			var matchCount = 0;
+			angular.forEach(genres, function(genre) {
+				if(filteredGenres.indexOf(genre) >= 0) {
+					console.log('matched genre ' + genre);
+					matchCount++;
+				}
+			});
+			if(matchCount == filteredGenres.length - 1) {
+				console.log('pushing rank: ' + rank.name);
+				filtered.push(rank);
+			}
+		});
+
+		return filtered;
+	}
 });
