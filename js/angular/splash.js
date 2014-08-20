b2d4bcf4ac396693589c562f85f199526a7ecab0
phonyphonecall <shendrickso2@wisc.edu>
2014-08-20 11:43:37 -0500

    [splash] angularize FBinit

--- js/angular/splash.js
+++ js/angular/splash.js
@@ -3,9 +3,9 @@ var stagegage = angular.module('stagegage', []);
 stagegage.controller('SplashController', function ($scope) {
   console.log('temporarily unauthorizing user');
   $scope.isAuthorized = false;
-});
 
-window.fbAsyncInit = function onFBLoad() {
+  
+  window.fbAsyncInit = function onFBLoad() {
     console.log('initializing FB');
     FB.init({
       appId      : '271053079752670',
@@ -13,27 +13,25 @@ window.fbAsyncInit = function onFBLoad() {
       xfbml      : true,
       version    : 'v2.0'
     });
-
-  console.log('In fbAsyncInit');
-  FB.getLoginStatus(function(response) {
-
-    var authOverlay = document.getElementById('auth-overlay');
-    if (response.status === 'connected') {
-      console.log('Authenticated user detected');
-
-      $scope.isAuthorized = true;
-      var uid = response.authResponse.userID;
-      // Log this shit (send UID to service)
-      var accessToken = response.authResponse.accessToken;
-      authOverlay.style.width = 0;
-      authOverlay.style.height = 0;
-      authOverlay.style.display = 'none';
-    } else {
-      console.log('Unauthorized user detected');
-      // Unauthorized user... 
-      authOverlay.style.width = 100;
-      authOverlay.style.height = 100;
-      authOverlay.style.display = 'inline';
-    }
-  });
-}
+    FB.getLoginStatus(function(response) {
+      var authOverlay = document.getElementById('auth-overlay');
+      if (response.status === 'connected') {
+        console.log('Authenticated user detected');
+        $scope.isAuthorized = true;
+        // Log this shit (send UID to service)
+        var uid = response.authResponse.userID;
+        console.log('userID = ' + uid);
+        var accessToken = response.authResponse.accessToken;
+        authOverlay.style.width = 0;
+        authOverlay.style.height = 0;
+        authOverlay.style.display = 'none';
+      } else {
+        console.log('Unauthorized user detected');
+        // Unauthorized user... 
+        authOverlay.style.width = 100;
+        authOverlay.style.height = 100;
+        authOverlay.style.display = 'inline';
+      }
+    });
+  }
+});
