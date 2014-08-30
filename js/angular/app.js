4fc0b3990a233db31c503369f888ff02845ab978
phonyphonecall <shendrickso2@wisc.edu>
2014-08-30 17:24:31 -0500

    rank creation select shows

--- js/angular/app.js
+++ js/angular/app.js
@@ -4,7 +4,8 @@ var stagegage = angular.module('stagegage', [
     'SplashControllers',
     'RankingsControllers',
     'AuthControllers',
-    'MenuControllers'
+    'MenuControllers',
+    'CreateRankControllers'
 ]);
 
 stagegage.config(['$routeProvider',
@@ -22,6 +23,14 @@ stagegage.config(['$routeProvider',
                 templateUrl: 'partials/rankings.html',
                 controller: 'RankingsController'
             })
+            .when('/create-rank/select-shows', {
+                templateUrl: 'partials/create-rank-select-shows.html',
+                controller: 'CreateRankController'
+            })
+            .when('/create-rank/rank/festival/:festivalName', {
+                templateUrl: 'partials/create-rank-rank.html',
+                controller: 'CreateRankController'
+            })
             .when('/splash', {
                 templateUrl: 'partials/splash.html',
                 controller: 'SplashController'
@@ -35,3 +44,50 @@ stagegage.config(['$routeProvider',
             });
     }
 ]);
+
+
+// https://gist.github.com/mbenford/8016984
+stagegage.directive('autosize', function($document) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            var placeholder, span, resize;
+
+            placeholder = element.attr('placeholder') || '';
+
+            span = angular.element('<span></span>');
+            span[0].style.cssText = getComputedStyle(element[0]).cssText;
+            span.css('display', 'none')
+                .css('visibility', 'hidden')
+                .css('width', 'auto');
+
+            $document.find('body').append(span);
+
+            resize = function(value) {
+                if(value != undefined) {
+                    if (value.length < placeholder.length) {
+                        value = placeholder;
+                    }
+                    span.text(value);
+                    span.css('display', '');
+                    try {
+                        element.css('width', span.prop('offsetWidth') + 60 + 'px');
+                    }
+                    finally {
+                        span.css('display', 'none');
+                    }
+                }
+            };
+
+            ctrl.$parsers.unshift(function(value) {
+                resize(value);
+                return value;
+            });
+
+            ctrl.$formatters.unshift(function(value) {
+                resize(value);
+                return value;
+            })
+        }
+    };
+});
